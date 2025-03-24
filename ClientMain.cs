05d16559017179c88bb2ecfe43f3dbb644f972a6
Maraxoni <maraxoni16@gmail.com>
2025-03-24 02:04:55 +0100

    Add project files.

---
+++ ClientMain.cs
@@ -0,0 +1,21 @@
+﻿using MyNamespace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+    class ClientMain
+    {
+        static void Main(string[] args)
+        {
+            var binding = new BasicHttpBinding();
+            var endpoint = new EndpointAddress("http://localhost:8080/HelloWorldService");
+            HelloWorldClient client = new HelloWorldClient(binding, endpoint);
+            Console.WriteLine(client.GetHelloWorldAsString("Sample"));
+        }
+    }
+}
